367f5132774835c7113f050e3ee2e4040ef8df36
Dan Banfield <dandbanfield@gmail.com>
2022-09-29 19:33:01 +0100

    Fix memory leak.

--- NintendoWare Audio Player/Audio/AudioPlayer.cs
+++ NintendoWare Audio Player/Audio/AudioPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Media;
 using VGAudio.Formats;
 using VGAudio.Containers.Wave;
@@ -10,20 +11,32 @@ namespace NintendoWare_Audio_Player.Audio
     {
         private static SoundPlayer soundPlayer;
 
+        private static byte[] bfstmBytes;
+        private static AudioData bfstmData;
+        private static byte[] wavBytes;
+
+        private static WaveWriter waveWriter;
+        private static BCFstmReader bcfstmReader;
+
         public static void PlayBFSTMFile(string fileLocation)
         {
-            if (soundPlayer == null)
-                soundPlayer = new SoundPlayer();
+            if (soundPlayer == null) soundPlayer = new SoundPlayer();
 
-            byte[] bfstmBytes = File.ReadAllBytes(fileLocation);
-            AudioData bfstmData = new BCFstmReader().Read(bfstmBytes);
-            byte[] wavBytes = new WaveWriter().GetFile(bfstmData);
+            bfstmBytes = File.ReadAllBytes(fileLocation);
+
+            if (bcfstmReader == null) bcfstmReader = new BCFstmReader();
+            bfstmData = bcfstmReader.Read(bfstmBytes);
+
+            if (waveWriter == null) waveWriter = new WaveWriter();
+            wavBytes = waveWriter.GetFile(bfstmData);
 
             using (MemoryStream memoryStream = new MemoryStream(wavBytes))
             {
                 soundPlayer.Stream = memoryStream;
-                soundPlayer.PlayLooping();
+                soundPlayer.Play();
             }
+
+            GC.Collect();
         }
     }
 }
